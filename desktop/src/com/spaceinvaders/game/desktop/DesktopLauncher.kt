c57b521997014c3e15021e1589c4133d093343b7
LucasAlegre <lucasnale@gmail.com>
2018-09-07 11:58:54 -0300

    Initial commit

---
+++ desktop/src/com/spaceinvaders/game/desktop/DesktopLauncher.kt
@@ -0,0 +1,13 @@
+package com.spaceinvaders.game.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.spaceinvaders.game.SpaceInvaders
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        LwjglApplication(SpaceInvaders(), config)
+    }
+}
