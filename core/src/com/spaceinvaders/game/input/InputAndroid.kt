7be878fb2b879934cf8006f7ff8a96c3e527ff91
LucasAlegre <lucasnale@gmail.com>
2018-11-27 22:50:32 -0200

    TouchPad Branch

--- core/src/com/spaceinvaders/game/input/InputAndroid.kt
+++ core/src/com/spaceinvaders/game/input/InputAndroid.kt
@@ -2,34 +2,53 @@ package com.spaceinvaders.game.input
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.math.Vector3
+import com.spaceinvaders.game.logic.GameLogic
+import com.spaceinvaders.game.screens.GameScreen
 import kotlin.math.abs
 
 class InputAndroid : InputHandler {
 
     private val accelerometerAvailable: Boolean = Gdx.input.isPeripheralAvailable(Input.Peripheral.Accelerometer)
+    private var touchPos: Vector3 = Vector3()
+    private val maxDistance: Float = 500f * Gdx.graphics.deltaTime
+
+    fun getDelta(): Vector2{
+        if(Gdx.input.isTouched){
+            touchPos.set(Gdx.input.x.toFloat(), Gdx.input.y.toFloat(), 0f)
+            GameScreen.camera.unproject(touchPos)
+            val tmp = Vector2()
+            tmp.set(touchPos.x, touchPos.y).sub(GameLogic.player.body.x, GameLogic.player.body.y)
+            if(tmp.len() < 15)
+                return Vector2(0f,0f)
+            return tmp
+        }
+        else return Vector2(0f,0f)
+    }
 
     override fun moveLeft(): Boolean{
-        return Gdx.input.accelerometerX > 0
+        return getDelta().x < 0
     }
 
     override fun moveRight(): Boolean{
-        return Gdx.input.accelerometerX < 0
+        return getDelta().x > 0
     }
 
     override fun moveUp(): Boolean{
-        return Gdx.input.accelerometerY < 5.5f
+        return getDelta().y > 0
     }
 
     override fun moveDown(): Boolean{
-        return Gdx.input.accelerometerY > 5.5f
+        return getDelta().y < 0
     }
 
     override fun horizontalSpeed(): Float {
-        return abs(Gdx.input.accelerometerX)
+        return abs(getDelta().nor().scl(maxDistance).x)
     }
 
     override fun verticalSpeed(): Float {
-        return abs(Gdx.input.accelerometerY - 5.5f)
+        return abs(getDelta().nor().scl(maxDistance).y)
     }
 
     override fun shoot(): Boolean{
