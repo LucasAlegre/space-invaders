3628e789cbc5351728d821d1e684f5785e0bd7d1
LucasAlegre <lucasnale@gmail.com>
2018-11-29 19:24:59 -0200

    Time at the end screen

--- core/src/com/spaceinvaders/game/entities/UFO.kt
+++ core/src/com/spaceinvaders/game/entities/UFO.kt
@@ -4,7 +4,7 @@ import com.badlogic.gdx.Gdx
 import com.spaceinvaders.game.screens.GameScreen
 
 
-class UFO(y: Float): Enemy(x=0f, y=y, width=140f, height=70f, texture= GameScreen.ufoTexture, speed=200f, score=150, lives=1, shootDelay=1f, timeInit=0f){
+class UFO(y: Float): Enemy(x=0f, y=y, width=140f, height=70f, texture= GameScreen.ufoTexture, speed=200f, score=300, lives=1, shootDelay=1f, timeInit=0f){
 
     override fun hasReachedRightLimit(): Boolean {
         if(super.hasReachedRightLimit()) {
